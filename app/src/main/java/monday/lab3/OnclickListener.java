cdceb7b957a0d3b9716ba8471eaa8870ce2483d7
Zishan Omar <touin.zishan@strathmore.edu>
2017-10-23 18:04:42 +0300

    first commit

---
+++ app/src/main/java/monday/lab3/OnclickListener.java
@@ -0,0 +1,8 @@
+package monday.lab3;
+
+/**
+ * Created by User on 10/23/2017.
+ */
+
+interface OnclickListener {
+}
---
+++ app/src/main/java/monday/lab3/Yoh.java
@@ -0,0 +1,10 @@
+package monday.lab3;
+
+import android.app.Activity;
+
+/**
+ * Created by User on 10/23/2017.
+ */
+
+public class Yoh extends Activity implements OnclickListener {
+}
